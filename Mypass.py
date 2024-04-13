18dba575546eb960257b729964da31ec1dfc0082
R2xdx <141178629+R2xdx@users.noreply.github.com>
2024-04-12 20:18:41 -0400

    Actualziaciones finales
    implementación loggings en el código mypass:py
    
    Eliminación versión experimental
    
    Desarrollo Readme

--- Mypass.py
+++ Mypass.py
@@ -9,6 +9,8 @@ import hashlib
 #generador contraseñas
 import random
 import string
+#logins
+import logging
 
 
 
@@ -59,7 +61,7 @@ def create_connection(db_file):
         conn = sqlite3.connect(db_file)
         return conn
     except Error as e:
-        print(e)
+        logging.error(e)
     return conn
 
 def create_table(conn, create_table_sql):
@@ -68,7 +70,7 @@ def create_table(conn, create_table_sql):
         c = conn.cursor()
         c.execute(create_table_sql)
     except Error as e:
-        print(e)
+        logging.error(e)
 
 
 #############------------------------CRUD USER------------------------#############
@@ -79,9 +81,9 @@ def insert_user(conn, id_user, nombre, main_password):
         c = conn.cursor()
         c.execute("INSERT INTO usuario (id_user, nombre, main_password) VALUES (?, ?, ?)", (id_user, nombre, main_password))
         conn.commit()  # Guardar los cambios en la base de datos
-        print("Usuario agregado correctamente.")
+        logging.info("Usuario agregado correctamente.")
     except Error as e:
-        print("Error al insertar usuario:", e)
+        logging.error("Error al insertar usuario:", e)
 
 def get_user(conn, id_user):
     """Obtener el nombre y contraseña de usuario basado en su ID"""
@@ -92,10 +94,10 @@ def get_user(conn, id_user):
         if row:
             return row
         else:
-            print("No se encontró ningún usuario con el ID proporcionado.")
+            logging.warning("No se encontró ningún usuario con el ID proporcionado.")
             return None
     except Error as e:
-        print("Error al obtener el nombre de usuario:", e)
+        logging.error("Error al obtener el nombre de usuario:", e)
         return None
     
 def count_users(conn):
@@ -174,20 +176,20 @@ def main():
         create_table(conn, sql_create_usuario)
         return conn 
     else:
-        print("Error! no se pudo crear la conexión a la base de datos.")
+        logging.error("Error! no se pudo crear la conexión a la base de datos.")
         return None 
 
 
 
-
+#configuracion logins
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 if __name__ == '__main__':
     conn = main()
+    logging.info("Conexión establecida correctamente.")
     #Programa como tal 
-
     if conn is not None:
-        print("Conexión establecida.")
 
 
         #Saber si hay un usuario registrado 
@@ -205,7 +207,7 @@ if __name__ == '__main__':
                 elif confirmacion == "SI":
                     corte = True
                 else:
-                    print("Entrada no valida.")
+                    print("Entrada no valida.")              
 
             corte = False
             while corte == False:
@@ -221,7 +223,7 @@ if __name__ == '__main__':
                     print("Contraseña registrada satisfactoriamente.\n")
 
             
-            insert_user(conn, 1, user, Hash_bd)
+            insert_user(conn, 1, user, Hash_bd) #el msj lo imprime la funcion directamente
 
 
         #Si hay un usuario
@@ -235,10 +237,10 @@ if __name__ == '__main__':
                 contra_bruto = input("Ingrese su contraseña porfavor: ")
                 Hash = generar_clave_SHA_256(contra_bruto)
                 if Hash == hash_bd:
-                    print("Contraseña correcta.\n")
+                    logging.info("Contraseña correcta.\n")
                     corte = True
                 else:
-                    print("Contraseña incorrecta, porfavor volver a intentar.\n")
+                    logging.info("Contraseña incorrecta, porfavor volver a intentar.\n")
             
             num_aux = (len(contra_bruto))//2
             key_master_pre = contra_bruto[:num_aux] + Hash + contra_bruto[num_aux:]
@@ -270,16 +272,16 @@ if __name__ == '__main__':
                             print("")
 
                             if contra_app == aux_app:
-                                print("Contraseña registrada satisfactoriamente\n")
+                                logging.info("Contraseña registrada satisfactoriamente\n")
                                 i = False
                                 contra_app_encrip = encriptar_texto(contra_app, key_master)
                                 insert_password(sitio_lower, contra_app_encrip)
 
                             else:
-                                print("Las contraseñas son diferentes,porfavor volver a intentar.\n")
+                                print("Las contraseñas son diferentes,porfavor volver a intentar.")
                     else:
                         print("")
-                        print("Ya hay una contraseña registrada para "+ sitio + ".\n")
+                        logging.info("Ya hay una contraseña registrada para "+ sitio + ".\n")
 
 
 
@@ -290,9 +292,10 @@ if __name__ == '__main__':
                     if view_password(sitio_lower):
                         contra_encriptada = view_password(sitio_lower)[1]
                         contra_de_vuelta = desencriptar_texto(contra_encriptada, key_master)
+                        logging.info("Contraseña encontrada:\n")
                         print("La contraseña para " + sitio +" es: " + contra_de_vuelta +"\n")
                     else:
-                        print(sitio + " no se ha encontrado en la base de datos.\n")
+                        logging.info(sitio + " no se ha encontrado en la base de datos.\n")
 
 
 
@@ -304,6 +307,7 @@ if __name__ == '__main__':
                     if view_password(sitio_lower):
                         contra_encriptada = view_password(sitio_lower)[1]
                         contra_de_vuelta = desencriptar_texto(contra_encriptada, key_master)
+                        logging.info("Contraseña encontrada:\n")
                         print("La contraseña para " + sitio +" es: " + contra_de_vuelta +"\n")
 
                         i = True
@@ -312,14 +316,15 @@ if __name__ == '__main__':
                             aux_app = input("Ingrese la contraseña nuevamente: ")
                             print("")
                             if contra_app == aux_app:
-                                print("Contraseña registrada satisfactoriamente\n")
+                                logging.info("Contraseña registrada satisfactoriamente\n")
                                 i = False
                                 contra_app_encrip = encriptar_texto(contra_app, key_master)
                                 update_password(sitio_lower, contra_app_encrip)
                             else:
                                 print("Las contraseñas son diferentes,porfavor volver a intentar.\n")
                     else:
-                        print(sitio + " no se ha encontrado en la base de datos.\n")   
+                        logging.info(sitio + " no se ha encontrado en la base de datos.\n")
+  
 
 
 
@@ -329,6 +334,7 @@ if __name__ == '__main__':
                     print("")
                     if view_password(sitio_lower):
                         i = True
+                        logging.info("Contraseña encontrada.\n")
                         while i:
                             confirmar = (input("¿Seguro que desea eliminar la contraseña de este sitio? (SI/NO): ")).upper()
                             if confirmar == "SI":
@@ -341,7 +347,7 @@ if __name__ == '__main__':
                             else:
                                 print("Entrada no valida, porfavor volver a confirmar o desconfirmar.\n")
                     else:
-                        print(sitio + " no se ha encontrado en la base de datos.\n")
+                        logging.info(sitio + " no se ha encontrado en la base de datos.\n")
 
 
 
@@ -353,12 +359,18 @@ if __name__ == '__main__':
                         if largo.isdigit():
                             largo = int(largo)
                             if largo >= 8:
-                                print("¿Desea que su conotraseña posea signos de puntuación?, ejemplo: @;'...")
-                                tipo = (input("(SI/NO): ")).upper()
-                                contra_nueva = generador_pass(largo, tipo)
-                                print("La contraseña generada es: "+ str(contra_nueva))
-                                print("")
-                                i = False
+                                flag = True
+                                while flag:
+                                    print("¿Desea que su conotraseña posea signos de puntuación?, ejemplo: @;'...")
+                                    tipo = (input("(SI/NO): ")).upper()
+                                    if tipo != "NO" and tipo != "SI":
+                                        print("Entrada no valida.")
+                                    else:
+                                        contra_nueva = generador_pass(largo, tipo)
+                                        print("La contraseña generada es: "+ str(contra_nueva))
+                                        print("")
+                                        i = False
+                                        flag = False
                             else:
                                 print("Largo muy corto.\n")
                         else: 
@@ -368,12 +380,12 @@ if __name__ == '__main__':
 
                 elif opcion == "6":
                     ciclo = False
+                    logging.info("Saliendo del programa.\n")
 
                 else:
                     print("Opción no valida, inserte de nuevo la opción a escojer de nuevo porfavor.\n")
 
         if count_users(conn) > 1:
-            print("Este programa tiene bloqueada la posibilidad de tener mas de un usuario y actualmente se encuentra en esa situación,") 
-            print("se tiene que borrar la base de datos debido a que no se puede asegurar la integridad de estos mismos")
+            logging.error("Este programa tiene bloqueada la posibilidad de tener mas de un usuario y actualmente se encuentra en esa situación, se tiene que borrar la base de datos debido a que no se puede asegurar la integridad de estos mismos")
     else:
-        print("Error al establecer la conexión.")
+        logging.error("Error al establecer la conexión.")
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# Pruebas-de-SW-T1
-Repositorio para la primera tarea de Inf 331
+# MyPass
+
+MyPass es una aplicación simple de gestión de contraseñas escrita en Python. Permite a los usuarios almacenar y gestionar sus contraseñas de forma segura en una base de datos SQLite. Utiliza técnicas de encriptación para proteger las contraseñas almacenadas.
+
+## Funcionalidades
+
+- **Registro de Usuarios:** Los usuarios pueden registrarse con un nombre de usuario y una contraseña maestra.
+- **Gestión de Contraseñas:** Los usuarios pueden almacenar, ver, actualizar y eliminar contraseñas para diferentes sitios o aplicaciones.
+- **Generador de Contraseñas:** La aplicación incluye un generador de contraseñas que permite a los usuarios crear contraseñas seguras con diferentes longitudes y opciones de caracteres.
+
+## Requisitos
+
+- Python 3.x
+- Biblioteca cryptography (instalable a través de pip: `pip install cryptography`)
+
+## Instalación
+
+1. Clona o descarga este repositorio en tu máquina local.
+2. Asegúrate de tener Python 3.x instalado en tu sistema.
+3. Instala la biblioteca cryptography utilizando pip: `pip install cryptography`.
+
+## Uso
+
+1. Ejecuta el archivo `mypass.py` para iniciar la aplicación.
+2. Sigue las instrucciones en pantalla para registrarte como usuario o acceder si ya tienes una cuenta.
+3. Una vez dentro, sigue las opciones del menú para gestionar tus contraseñas.
+
+## Estructura del Proyecto
+
+- **mypass.py:** El archivo principal que contiene el código fuente de la aplicación.
+- **mypass.db:** La base de datos SQLite donde se almacenan los usuarios y las contraseñas.
+- **README.md:** Este archivo que proporciona información sobre el proyecto.
+
+## Contribuir
+
+Si deseas contribuir a este proyecto, siéntete libre de abrir un issue o enviar un pull request.
+
+## Licencia
+
+Este proyecto está bajo la licencia [MIT License](LICENSE).
